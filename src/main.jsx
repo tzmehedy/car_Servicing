3d94de036d437077afed09ef9479e7ff60530808
unknown <mehedytouhidurzaman@gmail.com>
2024-10-22 19:55:31 +0600

    react router completed

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './Router/Router.jsx'
+import Root from './layout/Root.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <Root></Root>
+    </RouterProvider>
   </StrictMode>,
 )
