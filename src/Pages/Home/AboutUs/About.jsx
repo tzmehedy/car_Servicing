cbf34434c3cee073baca6b2f9d477109790a01b5
unknown <mehedytouhidurzaman@gmail.com>
2024-10-23 19:40:59 +0600

    About us section in home page completed

---
+++ src/Pages/Home/AboutUs/About.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import aboutUsImg from "../../../assets/images/about_us/person.jpg"
+import aboutUsParts from "../../../assets/images/about_us/parts.jpg"
+
+const About = () => {
+    return (
+      <div className="mt-24">
+        <div className="flex flex-col md:flex-row  gap-5">
+          <div className="w-1/2 relative">
+            <img className="w-2/3" src={aboutUsImg} alt="" />
+            <img className="w-1/3 absolute right-36 top-48 border-4 border-white" src={aboutUsParts} alt="" />
+          </div>
+          <div className="w-1/2 space-y-3">
+            <p className="text-[#FF3811] font-bold">About Us</p>
+            <h1 className="font-bold text-3xl">
+              We are qualified & of experience in this field.
+            </h1>
+            <p className="text-justify">
+              There are many variations of passages of Lorem Ipsum available,
+              but the majority have suffered alteration in some form, by
+              injected humour, or randomised words which don't look even
+              slightly believable. <br />
+              There are many variations of passages of Lorem Ipsum available,
+              but the majority have suffered alteration in some form, by
+              injected humour, or randomised words which don't look even
+              slightly believable.
+            </p>
+            <button className="btn bg-[#FF3811] text-white font-bold">
+              Get More Info
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default About;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../../Components/Hero';
+import About from './AboutUs/About';
 
 const Home = () => {
     return (
         <div className='mt-10'>
             <Hero></Hero>
+            <About></About>
         </div>
     );
 };
